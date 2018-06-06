a5ce60a6244446da7fc360aee20691262a2eb76e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 18:22:27 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,7 +11,7 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    self.count(:genre)
+    self.songs.count(:genre)
     #return the number of genres associated with the artist
   end
 end
